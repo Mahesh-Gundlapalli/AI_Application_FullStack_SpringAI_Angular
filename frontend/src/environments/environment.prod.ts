ca57a9c9bd994dd1687e9c8e07272ac1a388453d
Mahesh-Gundlapalli <157827335+Mahesh-Gundlapalli@users.noreply.github.com>
2026-03-18 16:29:01 +0530

    update URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://spring-ai-docker-host.onrender.com/api/v1',
-  apiBaseUrl2: 'http://localhost:8080/api/v1'
+  apiBaseUrl1: 'http://localhost:8080/api/v1'
 };
